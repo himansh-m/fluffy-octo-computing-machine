64f7a7b9222d3f9d73204874780bc37256e0251c
himansh-dev <himanshmewada1@gmail.com>
2025-12-23 21:43:59 +0530

    Restructured the files. Modified the README.

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -9,22 +9,22 @@ int main()
 	window.setPosition({ 13,13 });
 
 	sf::Image icon;
-	if (!icon.loadFromFile("E:/fluffy-octo-computing-machine/chess.png"))
+	if (!icon.loadFromFile("E:/fluffy-octo-computing-machine/Project1/chess.png"))
 		window.close(); 
 	window.setIcon(icon);
 
-	sf::Font font("E:/fluffy-octo-computing-machine/comic.ttf");
+	sf::Font font("E:/fluffy-octo-computing-machine/Project1/comic.ttf");
 	
 	sf::Angle reverse = sf::degrees(180);
 
-	sf::Texture boardload("E:/fluffy-octo-computing-machine/chessboard.jpg");
+	sf::Texture boardload("E:/fluffy-octo-computing-machine/Project1/chessboard.jpg");
 	sf::Sprite board(boardload);
 	board.setPosition({ 256,56 });
-	float offSetX = 256;
-	float offSetY = 56;
+	register float offSetX = 256;
+	register float offSetY = 56;
 
 
-	const sf::Texture pieces("E:/fluffy-octo-computing-machine/ChessPiecesArray.png");
+	const sf::Texture pieces("E:/fluffy-octo-computing-machine/Project1/ChessPiecesArray.png");
 	sf::Sprite whitePawn(pieces, { {300,60},{60,60} });
 	sf::Sprite blackPawn(pieces, { {300,0},{60,60} });
 	blackPawn.setRotation(reverse);
@@ -87,7 +87,7 @@ int main()
 		window.clear({ 230, 230, 230 });
 
 		window.draw(board);
-		int Xoff = 0, Yoff = 0;
+		register int Xoff = 0, Yoff = 0;
 		for (char x : start)
 		{
 			if (std::isalpha(x))
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # fluffy-octo-computing-machine
-Game Development
+A collection of my ideas.
 
 
 ## Project1
@@ -9,7 +9,7 @@ Game Development
 #### Notes ( Project1 )
 - The library used is SFML.
 - The ChessPiecesArray.png might be a little different from the standard. The reason being the black pieces weren't visible on the board.
-- If you want you can change the any png files with a suitable replacement. Just make sure the sizes are the same.
+- If you want you can change any png files with a suitable replacement. Just make sure the sizes are the same.
 - You can use the above FEN string simulator to turn your chess game into a GUI.
 - I have also loaded a font in there. If you want to write something on the screen the font is already loaded.
 
